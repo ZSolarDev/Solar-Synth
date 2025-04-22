43990486bb5bc1d1b7dffc3cb24ff1f174c7c9da
ZSolarDev <101154324+ZSolarDev@users.noreply.github.com>
2025-04-22 12:50:50 -0400

    Update libesper.cpp

--- hllibesper/libesper.cpp
+++ hllibesper/libesper.cpp
@@ -7,7 +7,7 @@
 
 cSampleCfg makeCSampleConfig(int length, int batches, int pitchLength, int markerLength, int pitch, int isVoiced, int isPlosive, int useVariance, float expectedPitch, float searchRange, int tempWidth)
 {
-        cSampleCfg sampleConfig = { length, batches };
+        cSampleCfg sampleConfig = { length, batches, pitchLength, markerLength, pitch, isVoiced, isPlosive, useVariance, expectedPitch, searchRange, tempWidth };
         return sampleConfig;
 }
 
@@ -16,3 +16,14 @@ cSample makeCSample(float* waveform, int* pitchDeltas int* pitchMarkers, char* p
         cSample sample = { waveform, pitchDeltas, pitchMarkers, pitchMarkerValidity, specharm, avgSpecharm, config };
         return sample;
 }
+
+segmentTiming makeSegmentTiming(int start1, int start2, int start3, ing end1, int end2, int end3, int windowStart, int windowEnd, int offset)
+{
+        segmentTiming segTiming = { start1, start2, start3, end1, end2, end3, windowStart, windowEnd, offset };
+        return segTiming
+}
+
+HL_PRIM void HL_NAME(resample_specharm)(loat* avgSpecharm, float* specharm, int length, float* steadiness, float spacing, int startCap, int endCap, float* output, int start1, int start2, int start3, ing end1, int end2, int end3, int windowStart, int windowEnd, int offset) {
+    return cfg.sampleRate;
+}
+DEFINE_PRIM(_I32, get_sample_rate);
