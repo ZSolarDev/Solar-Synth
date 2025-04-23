270f236f1f10d0e3b58572e661e18044a155a97f
ZSolarDev <101154324+ZSolarDev@users.noreply.github.com>
2025-04-23 09:00:52 -0400

    wdaw

--- hllibesper/libesper.cpp
+++ hllibesper/libesper.cpp
@@ -27,6 +27,36 @@ segmentTiming makeSegmentTiming(int start1, int start2, int start3, int end1, in
     return segTiming;
 }
 
+HL_PRIM void HL_NAME(setc_sample_waveform)(int index, float* waveform) {
+    cSamples[index].waveform = waveform;
+}
+DEFINE_PRIM(_VOID, setc_sample_waveform, _I32 _ARR);
+
+HL_PRIM void HL_NAME(setc_sample_pitchDeltas)(int index, int* pitchDeltas) {
+    cSamples[index].pitchDeltas = pitchDeltas;
+}
+DEFINE_PRIM(_VOID, setc_sample_pitchDeltas, _I32 _ARR);
+
+HL_PRIM void HL_NAME(setc_sample_pitchMarkers)(int index, int* pitchMarkers) {
+    cSamples[index].pitchMarkers = pitchMarkers;
+}
+DEFINE_PRIM(_VOID, setc_sample_pitchMarkers, _I32 _ARR);
+
+HL_PRIM void HL_NAME(setc_sample_waveform)(int index, float* waveform) {
+    cSamples[index].waveform = waveform;
+}
+DEFINE_PRIM(_VOID, setc_sample_waveform, _I32 _ARR);
+
+HL_PRIM void HL_NAME(setc_sample_specharm)(int index, float* specharm) {
+    cSamples[index].specharm = specharm;
+}
+DEFINE_PRIM(_VOID, setc_sample_specharm, _I32 _ARR);
+
+HL_PRIM void HL_NAME(setc_sample_avgSpecharm)(int index, float* avgSpecharm) {
+    cSamples[index].avgSpecharm = avgSpecharm;
+}
+DEFINE_PRIM(_VOID, setc_sample_avgSpecharm, _I32 _ARR);
+
 HL_PRIM void HL_NAME(pushc_sample)(float* waveform, int* pitchDeltas, int* pitchMarkers, vstring* pitchMarkerValidity, float* specharm, float* avgSpecharm, int length, int batches, int pitchLength, int markerLength, int pitch, int isVoiced, int isPlosive, int useVariance, float expectedPitch, float searchRange, int tempWidth) {
     const char* _pitchMarkerValidity = hl_to_utf8(pitchMarkerValidity->bytes);
     cSampleCfg sampleConfig = makeCSampleConfig(length, batches, pitchLength, markerLength, pitch, isVoiced, isPlosive, useVariance, expectedPitch, searchRange, tempWidth);
@@ -38,20 +68,26 @@ HL_PRIM void HL_NAME(pushc_sample)(float* waveform, int* pitchDeltas, int* pitch
     cSamples = newSamples;
     cSamplesCount = newSize;
 }
-DEFINE_PRIM(_VOID, pushc_sample, _ARR _ARR _ARR);
+DEFINE_PRIM(_VOID, pushc_sample, _ARR _ARR _ARR _STRING _ARR _ARR _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _F32 _F32 _I32);
 
 HL_PRIM void HL_NAME(clearc_samples)() {
     memset(cSamples, 0, sizeof(cSample) * cSamplesCount);
     cSamplesCount = 0;
 }
-DEFINE_PRIM(_VOID, clearc_samples, _ARR _ARR _I32 _ARR _F32 _I32 _I32 _ARR _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32);
+DEFINE_PRIM(_VOID, clearc_samples);
 
-HL_PRIM void HL_NAME(spec_calc)(float* waveform, int* pitchDeltas, int* pitchMarkers, char* pitchMarkerValidity, float* specharm, float* avgSpecharm, int length, int batches, int pitchLength, int markerLength, int pitch, int isVoiced, int isPlosive, int useVariance, float expectedPitch, float searchRange, int tempWidth) {
+HL_PRIM void HL_NAME(pitch_calc_fallback)() {
+    pitchCalcFallback(cSamples, cfg);
+}
+DEFINE_PRIM(_VOID, pitch_calc_fallback);
+
+HL_PRIM void HL_NAME(spec_calc)(float* waveform, int* pitchDeltas, int* pitchMarkers, vstring* pitchMarkerValidity, float* specharm, float* avgSpecharm, int length, int batches, int pitchLength, int markerLength, int pitch, int isVoiced, int isPlosive, int useVariance, float expectedPitch, float searchRange, int tempWidth) {
+    const char* _pitchMarkerValidity = hl_to_utf8(pitchMarkerValidity->bytes);
     cSampleCfg sampleConfig = makeCSampleConfig(length, batches, pitchLength, markerLength, pitch, isVoiced, isPlosive, useVariance, expectedPitch, searchRange, tempWidth);
-    cSample sample = makeCSample(waveform, pitchDeltas, pitchMarkers, pitchMarkerValidity, specharm, avgSpecharm, sampleConfig);
+    cSample sample = makeCSample(waveform, pitchDeltas, pitchMarkers, _pitchMarkerValidity, specharm, avgSpecharm, sampleConfig);
     specCalc(sample, cfg);
 }
-DEFINE_PRIM(_VOID, spec_calc, _ARR _ARR _I32 _ARR _F32 _I32 _I32 _ARR _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32);
+DEFINE_PRIM(_VOID, spec_calc, _ARR _ARR _ARR _STRING _ARR _ARR _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _I32 _F32 _F32 _I32);
 
 HL_PRIM void HL_NAME(resample_specharm)(float* avgSpecharm, float* specharm, int length, float* steadiness, float spacing, int startCap, int endCap, float* output, int start1, int start2, int start3, int end1, int end2, int end3, int windowStart, int windowEnd, int offset) {
     segmentTiming segTiming = makeSegmentTiming(start1, start2, start3, end1, end2, end3, windowStart, windowEnd, offset);
