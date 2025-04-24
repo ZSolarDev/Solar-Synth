16d2391da498412bbe67dc1a6dca85d8cdf43f88
ZSolarDev <101154324+ZSolarDev@users.noreply.github.com>
2025-04-24 16:35:19 -0400

    segment timing

---
+++ hllibesper/default/source/libesper/SegmentTiming.hx
@@ -0,0 +1,11 @@
+typedef SegmentTiming = {
+    var start1:Int;
+    var start2:Int;
+    var start3:Int;
+    var end1:Int;
+    var end2:Int;
+    var end3:Int;
+    var windowStart:Int;
+    var windowEnd:Int;
+    var offset:Int;
+}
