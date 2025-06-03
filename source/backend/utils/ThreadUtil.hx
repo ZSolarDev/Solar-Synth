ef099843651c1681be0b516734b2834c75562223
ZSolarDev <101154324+ZSolarDev@users.noreply.github.com>
2025-06-03 10:29:29 -0400

    Update ThreadUtil.hx

--- source/backend/utils/ThreadUtil.hx
+++ source/backend/utils/ThreadUtil.hx
@@ -1,9 +1,7 @@
 package backend.utils;
 
-#if hl
 import hlwnative.HLApplicationStatus;
 import sys.thread.Thread;
-#end
 
 class ThreadUtil
 {
@@ -12,14 +10,13 @@ class ThreadUtil
 	private static var threadsUsed:Int = 1; // One is always used as the main thread
 
 	static function get_totalThreads():Int
-		return #if hl cast HLApplicationStatus.getTotalThreads(); #else 1 #end
+		return cast HLApplicationStatus.getTotalThreads();
 
 	static function get_freeThreads():Int
 		return totalThreads - threadsUsed;
 
 	public static function createThread(job:() -> Void)
 	{
-		#if hl
 		if (freeThreads > 0)
 		{
 			threadsUsed++;
@@ -29,8 +26,5 @@ class ThreadUtil
 				threadsUsed--;
 			});
 		}
-		#else
-		job();
-		#end
 	}
 }
